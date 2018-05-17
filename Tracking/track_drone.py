9366867bf2c1b650905c4e834ffe4ed0f4ace095
Jyvais <39129737+Jyvais@users.noreply.github.com>
2018-05-17 10:26:47 +0200

    Update track_drone.py

--- Tracking/track_drone.py
+++ Tracking/track_drone.py
@@ -9,12 +9,27 @@ import imutils
 import cv2
 import itertools
 from math import atan2,degrees
+import freenect
 
 
 
-def areAligned(l):
-     produit_croix=abs((c[2][1]-c[0][1])*(c[1][0]-c[0][0])-(c[2][0]-c[0][0])*(c[1][1]-c[0][1]))
-     return (produit_croix<2000)
+def get_depth():
+    return freenect.sync_get_depth()[0]
+
+
+def get_video():
+    return freenect.sync_get_video()[0][:, :, ::-1]  # RGB -> BGR
+
+
+def getAligned(centroidList):
+     min=2000
+     cmin=None
+     for c in itertools.permutations(centroidList,3):
+          produit_croix=abs((c[2][1]-c[0][1])*(c[1][0]-c[0][0])-(c[2][0]-c[0][0])*(c[1][1]-c[0][1]))
+	  if(produit_croix<min):
+	       cmin=c
+	       min=produit_croix
+     return (cmin)
 
 
 #returns the square of the AB distance
@@ -38,7 +53,7 @@ def getCenterIndex(l):
 def getAngleOfLineBetweenTwoPoints(p1, p2):
     xDiff = p2[0] - p1[0]
     yDiff = p2[1] - p1[1]
-    return degrees(atan2(yDiff, xDiff)-np.pi/2.)#probleme : va de -270 a +90
+    return degrees(atan2(yDiff, xDiff))#eventuellement -np.pi/2.
 
 
 #place the points in the right order : first the center and last the bottom point
@@ -49,17 +64,8 @@ def getFigure(c, centroidList):
      (centerIndex,maxDistSquared)=getCenterIndex(c)  
      
      #put the center at the beginning of the list c 
-     c[0],c[centerIndex]=c[centerIndex],c[0]  
-     
-     """
-     Focal length F:
-     F = (Perceived lenght in px * Height in cm) / real Length in cm 
-     attention notre distance est au carré !
-     
-     pour calculer ensuite : 
-     H = F * L / P
-     """
-    
+     c[0],c[centerIndex]=c[centerIndex],c[0]      
+
      for i in range(4):
          if centroidList[i] not in c:
               bottomIndex=i 
@@ -67,6 +73,11 @@ def getFigure(c, centroidList):
      #add the bottom point at the end of centroidList
      c.append(centroidList[bottomIndex])
      angle = getAngleOfLineBetweenTwoPoints(c[0],c[3])
+     
+     #put the right point at c[1], left at c[2]
+     if(0<(c[3][0]-c[0][0])*(c[1][1]-c[0][1])-(c[3][1]-c[0][1])*(c[1][0]-c[0][0])):
+          c[1],c[2]=c[2],c[1]
+     
      return (c,maxDistSquared,angle)
       
 
@@ -76,57 +87,35 @@ def getFigure(c, centroidList):
                      
 
 if __name__=="__main__":
-    
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-v", "--video",
-    	help="path to the (optional) video file")
-
-    args = vars(ap.parse_args())
-    
     # define the lower and upper boundaries of the "green"
     # ball in the HSV color space
-    greenLower = (25, 70, 6)#(29, 86, 6)
-    greenUpper = (80, 255, 255)#(64, 255, 255)
+    greenLower = (55, 50, 6)
+	#pas restrictif 25 50 6
+	#tres restrictif : 50 80 30
+    greenUpper = (85, 255, 255)
 
-    
-    # if a video path was not supplied, grab the reference
-    # to the webcam
-    if not args.get("video", False):
-        camera = cv2.VideoCapture(0)
-        frame_width = int(camera.get(3))
-        frame_height = int(camera.get(4))
-    
-    # otherwise, grab a reference to the video file
-    else:
-        camera = cv2.VideoCapture(args["video"])
-     
-    # Check if camera opened successfully
-    if (camera.isOpened() == False): 
-        print("Unable to read camera feed")
-     
-    # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-    out = cv2.VideoWriter('output.avi',cv2.cv.CV_FOURCC('M','J','P','G'), 10, (600,600*frame_height/frame_width))
-    # comme on va resize la width à 600, la heigth va suivre : ancienne_height*600/ancienne_width
+    resize_width=900 #original is 640
+    frame=frame=get_video()
+    frame = imutils.resize(frame, width=resize_width)
+    frame_width = resize_width
+
+
+     # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+    out = cv2.VideoWriter('output.avi',cv2.cv.CV_FOURCC('M','J','P','G'), 10, (frame_width,frame_width))
+    # comme on va resize la width a 600, la heigth va suivre : ancienne_height*600/ancienne_width
     
     
     # keep looping until break
     while True:
-        # grab the current frame
-        (grabbed, frame) = camera.read()
-    
-        # if we are viewing a video and we did not grab a frame,
-        # then we have reached the end of the video
-        if args.get("video") and not grabbed:
-            print("break")
-            break
+	frame=get_video()
+	depth_frame = get_depth()
+        # resize the frame
+        frame = imutils.resize(frame, width=resize_width)
 
-        # resize the frame, blur it, and convert it to the HSV color space
-        """                
-        utile??????????????????
-        """
-        frame = imutils.resize(frame, width=600)
-        # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	#brouiller l'image ne change pas grand chose, a tester...
+        #blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+
+	#convert it to the HSV color space
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
         # construct a mask for the color "green", then perform
@@ -140,6 +129,7 @@ if __name__=="__main__":
         # (x, y) center of the ball
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
     		 cv2.CHAIN_APPROX_SIMPLE)[-2]
+	#cv2.drawContours(frame,cnts,-1, (0,0,255),1)
         centroid = None
         centroidList=[]
         
@@ -159,27 +149,50 @@ if __name__=="__main__":
                 
     
             	  # only proceed if the radius meets a minimum size
-                if radius > 10:
-                    # draw the circle and centroid on the frame,
-                    cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-                    #cv2.circle(mask, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                if radius > 1:
+		    #print(hsv[int(y)][int(x)])
+                    
+		    #draw the circle and centroid on the frame,
+                    #cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
                     #cv2.circle(frame, centroid, 5, (0, 0, 255), -1)
-                    #add the centroid to the list
+                    
+		    #add the centroid to the list
                     centroidList.append(centroid)
                    
                    
             if len(centroidList)==4:
-                for c in itertools.permutations(centroidList,3):
-                    if areAligned(c):
-                         (centroidList,maxDistSquared,angle)=getFigure(list(c),list(centroidList))
+                aligned=getAligned(centroidList)
+                if(aligned!=None):    
+                         (centroidList,maxDistSquared,angle)=getFigure(list(aligned),list(centroidList))
                          """   position in centroidlist :
-                                 ?   0   ?
+                                 2   0   1
                                      3
-                         """   
-                         
+                         """                            
+
+ 			 #get the depth of each centroid
+			 depth_frame = imutils.resize(depth_frame, width=resize_width)
+			 depths=[]
+			 for i in centroidList:
+			       depths.append(depth_frame[i[1]][i[0]])
+			 
+			 
+			 #compute the angles of the drone
+			 roulis=(int(depths[1])-int(depths[2]))
+			 tangage=(int(depths[0])-int(depths[3]))*2
+			 distance=int(depths[0])
+
+
                          #print all useful information
                          cv2.putText(frame,
-                                'taille : '+str(maxDistSquared), 
+                                'centre : '+str(centroidList[0]), 
+                                (10,30), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 
+                                0.5,#font size
+                                (0,0,0),
+                                2)#line type
+
+                         cv2.putText(frame,
+                                'distance : '+str(depths[0]), 
                                 (10,50), 
                                 cv2.FONT_HERSHEY_SIMPLEX, 
                                 0.5,#font size
@@ -187,27 +200,41 @@ if __name__=="__main__":
                                 2)#line type
                          
                          cv2.putText(frame,
-                                'angle : '+str(angle), 
+                                'roulis : '+str(roulis), 
                                 (10,70), 
                                 cv2.FONT_HERSHEY_SIMPLEX, 
                                 0.5,#font size
                                 (0,0,0),
-                                2)#line type                     
+                                2)#line type   
+
+			 cv2.putText(frame,
+                                'tangage : '+str(tangage), 
+                                (10,90), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 
+                                0.5,#font size
+                                (0,0,0),
+                                2)#line type
+			 cv2.putText(frame,
+                                'lacet : '+str(angle), 
+                                (10,110), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 
+                                0.5,#font size
+                                (0,0,0),
+                                2)#line type                    
                          
-                         cv2.line(frame, centroidList[0], centroidList[2], (0,255,0),2) # BGR order
-                         cv2.circle(frame, centroidList[0], 5, ( 255, 0,0), -1)#bleu foncé
-                         cv2.circle(frame, centroidList[3], 5, ( 220, 200,0), -1)#bleu clair
-                         """
-                         cv2.line(mask, centroidList[0], centroidList[2], (0,255,0),2) # BGR order
-                         cv2.circle(mask, centroidList[0], 5, ( 255, 0,0), -1)#bleu foncé
-                         cv2.circle(mask, centroidList[3], 5, ( 220, 200,100), -1)#bleu clair
-                         """
-                         #break?????
-                        
-                        
-                        
+                         #cv2.line(frame, centroidList[1], centroidList[2], (0,255,0),2) # BGR order
+                         cv2.circle(frame, centroidList[0], 2, ( 255,0,0 ), -1)#blue : center
+			 cv2.circle(frame, centroidList[1], 2, ( 0,255,0 ), -1)#green : right
+                         cv2.circle(frame, centroidList[3], 2, ( 0,0,255 ), -1)#Red : bottom
+
+			 
+
+                         
+			
+			       
         # write the frame to output video
-        out.write(frame)
+        out.write(frame)                 
+       			 
         # show the frame to our screen
         cv2.imshow("Frame", frame)
         #cv2.imshow("Frame", mask)
@@ -221,5 +248,5 @@ if __name__=="__main__":
     out.release()
     
     # cleanup the camera and close any open windows
-    camera.release()
+    
     cv2.destroyAllWindows()
